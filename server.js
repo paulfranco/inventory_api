53a565619ffede6b74f8b2c92eee7b79d9f81fe0
Paul Franco <mail@paulfran.co>
2021-01-18 17:35:17 -0800

    installed colors, fixed issue with dotenv

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+//Load env vars
+dotenv.config({ path: './config/config.env'});
+
+// Connect to database
+connectDB();
 
 // Route files
 const warehouses = require('./routes/warehouses');
+const { getWarehouses } = require('./controllers/warehouses');
 
-//Load env vars
-dotenv.config({ path: '/config/config.env'});
 
 const app = express();
 
@@ -16,10 +22,16 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Mount router
-app.use('/api/v1/warehouses');
+app.use('/api/v1/warehouses', getWarehouses);
 
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on(`unhandledRejection`, (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1))
+});
