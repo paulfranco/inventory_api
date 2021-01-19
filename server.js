09fe869b6f8e27719f75e60dbcd783666c656efc
Paul Franco <mail@paulfran.co>
2021-01-18 17:38:08 -0800

    import colors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const colors = require('colors');
 
 //Load env vars
 dotenv.config({ path: './config/config.env'});
@@ -28,10 +29,10 @@ app.use('/api/v1/warehouses', getWarehouses);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 // Handle unhandled promise rejections
 process.on(`unhandledRejection`, (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red.bold);
     server.close(() => process.exit(1))
 });
